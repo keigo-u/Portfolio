bc290797dc6ca7b0b0de29230f7a78eb782b50e9
keigo-u <keigo.u88@gmail.com>
2023-10-09 16:56:17 +0900

    frist commit

---
+++ src/components/LifeChart.tsx
@@ -0,0 +1,22 @@
+'use client'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+const LifeChart = () => {
+    return (
+        <div>
+            グラフ
+        </div>
+    )
+}
+
+export default LifeChart
